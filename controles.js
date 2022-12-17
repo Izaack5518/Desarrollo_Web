31252a54e075f91a9029f062836d18dc05ab3cbb
Izaack5518 <118235590+Izaack5518@users.noreply.github.com>
2022-12-17 13:28:57 -0600

    Add files via upload

---
+++ controles.js
@@ -0,0 +1,54 @@
+var saludo = "Hola jesus";
+console.log(saludo);
+
+let edad = 26;
+console.log("mi edad es de: "+edad+" años");
+
+let peso = 70.36;
+console.log("Mi peso es de: " + peso + " KG");
+
+let verdadero = true;
+let falso = false;
+
+console.log(verdadero);
+console.log(falso);
+
+let inde = undefined;
+console.log(undefined);
+
+let nulo = null;
+console.log(nulo);
+
+
+x=98.9;
+y=90.8;
+z=2
+console.log(x+y+(z*y));
+
+nombre = prompt("Ingrese su nombre: ");
+sueldo  = prompt("Ingrese su sueldo: ");
+
+if (sueldo >=10000){
+    document.write("Hola " + nombre);
+    document.write(" no merece un aumento");
+
+}else{
+    document.write("Hola " + nombre);
+    document.write(" merece un aumento");
+}
+
+p= true;
+n= false;
+valor = prompt("Ingrese un valor numerico");
+
+if(valor >=1){
+    document.write("El numero en positivo: "+ p);
+}else if( valor <0){
+    document.write("El numro es negativo: " + n);
+} else{
+    document.write("El valor es 0 ");
+}
+
+
+
+
